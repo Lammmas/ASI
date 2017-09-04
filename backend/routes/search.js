d020c0c13ef750d0a0ac249faa54448514bbada2
Renna <thetinysheep@gmail.com>
2017-09-04 10:32:56 +0300

    Forgot the order/sort

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/:name', function(req, res, next) {
     var username = req.params.name;
+    var result = [];
     // Because who needs whitespaces
     username = username.replace(/\s+/g, '');
 
@@ -24,11 +25,11 @@ router.get('/:name', function(req, res, next) {
     };
 
     // I copied this over from the task repo because it's such a straightforward thing, and it saved me typing :)
-    fetch(`https://api.github.com/users/${username}/repos`).then((response) => {
+    fetch(`https://api.github.com/users/${username}/repos?order=desc&sort=size`).then((response) => {
       response.json().then(body => {
         if (response.status === 200) {
           res.status = 200;
-          res.json(body);
+          res.json(body.slice(0, 5));
         } else if (response.status === 404) {
           res.status = 400;
           res.json({ error: 'User could not be found' });
